9e5664883900d83f26ec3efdcbbcb651d8eeca01
Sheri <100845274+sheri-shoukat@users.noreply.github.com>
2022-05-05 05:03:26 +0500

    creating main java program

--- project/data/img/src/Main.java
+++ project/data/img/src/Main.java
@@ -1,1 +1,162 @@
+java.util.ArrayList;
+java.util.LinkedList;
+
+
+public class LinkedListTest {
+LinkedListTest infixstorage;
+
+public Calculator(){
+    infixstorage = new LinkedListTest();
+}
+
+public static LinkedListTest ReadInFile(String path){
+    File file = new File(path);
+    LinkedListTest list = new LinkedListTest();
+
+    try {
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+           
+            while (st.hasMoreTokens()){
+                list.insert(st.nextToken());
+            }
+        }
+        scanner.close();
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+    return list;
+}
+
+}
+public class Treeset{
+
+public class StringTokenizerToArray {
+
+
+public class Main {
+public static void main(String[] args) {
+
+  String[] tokenArray = getTokensArray("Java StringTokenizer to Array", " ");
+        
+        if(tokenArray != null) {
+            
+            for(int i = 0 ; i < tokenArray.length ; i++) {
+                System.out.println( "Index: " + i + ", token: " + tokenArray[i] );
+            }
+        }        
+}
+    Set<String> ts1 = new TreeSet<>();
+ 
+        ts1.add("A");
+        ts1.add("B");
+        ts1.add("C");
+        ts1.add("D");
+
+        System.out.println(ts1);
+    }
+}
+}
+	
+        System.out.println("Input a string ");
+	StringTokenizer data = new StringTokenizer(keyboardInput.readLine());
+
+	
+	numberOfTokens = data.countTokens();
+	System.out.println("Number of tokens are = " + numberOfTokens + "\n");
+	
+	
+	for (int counter=0; counter < numberOfTokens; counter++) { 
+	    System.out.println(data.nextToken());
+	    }	
+
+	File emptyFile = new File("empty.txt");
+    Scanner emptyInput = new Scanner(emptyFile);
+
+    if(!emptyFile.exists()) {
+      System.out.println("The empty file doesn't exist.");
+      System.exit(0);
+    }
+
+    empty[] emp = new empty[count];
+	while (emptyrInput.hasNextLine()) {
+      count++;
+      emptyInput.nextLine();
+    }
+
+
+	File largeFile = new File("large.txt");
+    Scanner largeInput = new Scanner(largeFile);
+
+    if(!largeFile.exists()) {
+      System.out.println("The large file doesn't exist.");
+      System.exit(0);
+    }
+
+    large[] emp = new large[count];
+	while (largeInput.hasNextLine()) {
+      count++;
+      largeInput.nextLine();
+    }
+
+
+	
+	File mediumFile = new File("medium.txt");
+    Scanner mediumInput = new Scanner(mediumFile);
+
+    if(!mediumFile.exists()) {
+      System.out.println("The medium file doesn't exist.");
+      System.exit(0);
+    }
+
+    medium[] emp = new medium[count];
+	while (mediumInput.hasNextLine()) {
+      count++;
+      mediumInput.nextLine();
+    }
+
+	
+	File nameFile = new File("name.txt");
+    Scanner nameInput = new Scanner(nameFile);
+    if(!nameFile.exists()) {
+      System.out.println("The name file doesn't exist.");
+      System.exit(0);
+    }
+   name[]] emp = new name[count];
+	while (nameInput.hasNextLine()) {
+      count++;
+      nameInput.nextLine();
+    }
+	
+	File smallFile = new File("small.txt");
+    Scanner smallInput = new Scanner(smallFile);
+    
+    if(!smallFile.exists()) {
+      System.out.println("The small file doesn't exist.");
+      System.exit(0);
+    }
+   
+    small[] emp = new small[count];
+	while (smallInput.hasNextLine()) {
+      count++;
+      smallInput.nextLine();
+    }
+
+
+	File xlFile = new File("xl.txt");
+    Scanner xlInput = new Scanner(xlFile);
+    if(!xlFile.exists()) {
+      System.out.println("The xl file doesn't exist.");
+      System.exit(0);
+    }
+   
+    xl[] emp = new xl[count];
+	while (xlInput.hasNextLine()) {
+      count++;
+      xlInput.nextLine();
+    }
+}
+   }    
+
 
